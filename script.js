954f04374d10106523f9a91f3fa3bdf77f1e2db4
Hein Htet Aung <142594340+heinhtetaungyi@users.noreply.github.com>
2023-12-18 22:06:30 +0630

    Update Profile 1.0

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-
-
-function viewLink(url){
-  window.location.href = url;
+function viewLink(url) {
+ window.location.href = url;
 }
+
+function showAnim() {
+ let anim = document.querySelector(".overlay");
+ anim.style.display = "none";
+ setTimeout(function() {
+  anim.style.display = "flex";
+ }, 2000);
+}
+//showAnim();
